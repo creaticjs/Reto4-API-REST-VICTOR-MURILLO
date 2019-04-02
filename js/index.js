e110b646237793a1cb8afc3988e1a6b58a6f44e5
Victor Manuel Murillo Camayo <mcvictor@unicauca.edu.co>
2019-04-02 14:33:17 -0500

    Consulta de pelicula por Titulo de pelicula

--- js/index.js
+++ js/index.js
@@ -1,16 +1,4 @@
 /*
-traer imagenes
-https://api.themoviedb.org/3/movie/157336/images?api_key=d2f75c50a366b48f468d9a270511e992
-
-http://image.tmdb.org/t/p/w185/ + ruta
-doc:https://developers.themoviedb.org/3/getting-started/images
-examples:https://www.themoviedb.org/documentation/api/discover?language=es-ES
-*/
-
-/*
-inicializar Jqery
-
-
 */
 
 $(document).ready(function(){
@@ -20,10 +8,12 @@ $(document).ready(function(){
 const URL_API = "https://api.themoviedb.org/3/movie/550?"
 const API_KEY = "api_key=d2f75c50a366b48f468d9a270511e992"
 const IMG_URL = "http://image.tmdb.org/t/p/original/"
-
-
+const URL_QUERY_MOVIE = "https://api.themoviedb.org/3/search/movie?api_key=d2f75c50a366b48f468d9a270511e992&query="
 const URL_MOVIES_POPULARES ="https://api.themoviedb.org/3/discover/movie?api_key=d2f75c50a366b48f468d9a270511e992&sort_by=popularity.desc"
-const CLASE_CAROUSEL = "carousel-item"
+
+function peliculasPopulares() {
+   getData(URL_MOVIES_POPULARES).then(recorrerArrayPeliculasPopulares).catch()
+}
 
 function getData(url){
     return new Promise(function(resolve,reject){
@@ -42,59 +32,9 @@ function getData(url){
     })
 }
 
-async function enviarPromesas(){
-    var arrUrl = obtenerArrayUrl()
-    var promesas = arrUrl.map(function(url){
-        return getData(url)
-    })
-
-    var datos = await Promise.all(promesas)
-    datos.forEach(movie => {
-        mostrarPersonaje(movie)
-    });
-    console.log(datos)
-}
-
-function obtenerArrayUrl(){
-    var vector = []
-    for (let index = 0; index <= 5; index++) {
-         vector.push(URL_API+API_KEY)
-    }
-    return vector
-}
-function mostrarPersonaje(movie){
-    document.getElementById('contenedor').innerHTML += `
-    <div class="col-md rowPlus">
-    <div class="card" style="width: 18rem;">
-        <img src=${ IMG_URL+ movie.poster_path} class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${movie.original_title}</h5>
-                <p class="card-text">${movie.overview}</p>
-            </div>
-        </div>
-    </div>
-    `
-}
-
-
-
-
-
-/*
-Consultar las peliculas mas populares
-https://api.themoviedb.org/3/discover/movie?api_key=d2f75c50a366b48f468d9a270511e992&sort_by=popularity.desc
-
-*/
-
-
-function peliculasPopulares() {
-   getData(URL_MOVIES_POPULARES).then(recorrerArrayPeliculasPopulares).catch()
-}
-
 function recorrerArrayPeliculasPopulares(vector){
     vector.results.forEach((movie,index) => { 
-        console.log(index)
-       if(index===1){
+        if(index===1){
         document.getElementById('lista-carousel').innerHTML +=`
         <div class="carousel-item active">
             <img src=${IMG_URL+ movie.poster_path} class="d-block w-100" alt="...">
@@ -135,37 +75,35 @@ function recorrerArrayPeliculasPopulares(vector){
     });
 }
 
-
-
-function nombre(n){
-console.log(n)
-}
-
 peliculasPopulares()
 
 
 /*
 Uso de jquery para ocultar el (main) que tiene las peliculas populares
-para que se pueda hacer una busqueda
+para que se pueda hacer una busqueda Por nombre de pelicula
 
 */
 
 $('#btnBuscarTituloMovie').click(function(){
     $('#peliculasPopulares').hide('slow');
-    //hacer consulta a la API
-    getData(URL_API+API_KEY).then(mostrarPelicula)
+    var nombrePelicula = $('#inputTituloMovie').val()
+    getData(URL_QUERY_MOVIE+nombrePelicula,nombrePelicula).then(recorrerArrayPeliculaConsultada)
 })
 
-function mostrarPelicula(movie){
-    document.getElementById('peliculaConsultada').innerHTML +=`
+function recorrerArrayPeliculaConsultada(vector){
+    $('#peliculaConsultada').html(` 
     <div class="container">
-            <div class="row">
-                <div class="col text-center text-uppercase">
-                    <h2>Resultado</h2>
-                </div>
+        <div class="row">
+            <div class="col text-center text-uppercase">
+                <small> Peliculas</small>
+                <h2> Relacionadas</h2>
             </div>
-    </div>
-    <div class="col-6 offset-md-3 mt-4">
+        </div>
+    </div>`) 
+   
+    vector.results.forEach((movie,index) => { 
+    document.getElementById('peliculaConsultada').innerHTML +=`
+    <div class="col-6 mt-4">
         <div class="card mb-3" >
             <div class="row no-gutters">
                 <div class="col-md-4">
@@ -182,4 +120,8 @@ function mostrarPelicula(movie){
         </div>
     </div>
     `     
+    });
 }
+
+
+//Mostrar peliculas Populares despues de haber estado ocultas
